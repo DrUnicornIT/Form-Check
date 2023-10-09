57e93b7d48dce84a7d717c3a2028c12d8a9a8e1a
Cong Nguyen <62993074+DrUnicornIT@users.noreply.github.com>
2023-10-09 18:19:22 +0700

    Add files via upload

--- script_output.js
+++ script_output.js
@@ -17,15 +17,12 @@ document.getElementById("Describe").innerHTML = "Describe: " + des;
 var des_1=localStorage.getItem("pic1");
 document.getElementById("pic1").src = des_1;
 
-console.log(des_1);
-console.log(document.getElementById("pic2").src)
-
 var des_2=localStorage.getItem("pic2");
 document.getElementById("pic2").src = des_2;
 
-var des_3=localStorage.getItem("pic1");
+var des_3=localStorage.getItem("pic3");
 document.getElementById("pic3").src = des_3;
 
-var des_4=localStorage.getItem("pic1");
+var des_4=localStorage.getItem("pic4");
 document.getElementById("pic4").src = des_4;
 
